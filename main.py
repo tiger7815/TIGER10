8e41c6d390d2e181c895286158ea0e0ea4d7b6fb
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-11-27 01:25:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ import os
 bot = Client(
     "bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
+    api_id=int(os.environ.get("API_ID", 22609670)),
     api_hash=os.environ.get("API_HASH")
 )
 
